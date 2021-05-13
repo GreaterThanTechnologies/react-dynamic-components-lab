cb344021dc0580e6250837992cdcf52958717290
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-05-12 20:26:56 -0400

    learn test would not submit my solution, this is my solution so I can turn in my work.

---
+++ build/precache-manifest.8e9abf43233e94f4f90e86ca39d00c26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b53cb23b298083b32dd",
+    "url": "/static/css/main.eb134d74.chunk.css"
+  },
+  {
+    "revision": "9b53cb23b298083b32dd",
+    "url": "/static/js/main.626c9d26.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/static/js/2.345f8e30.chunk.js"
+  },
+  {
+    "revision": "92666593b89654e61d58cfbbed05f87b",
+    "url": "/index.html"
+  }
+];
